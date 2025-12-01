5e39918ac50534f7f344c54d8efd56b5e13120bb
andy8O <159593601+andy8O@users.noreply.github.com>
2025-12-01 16:56:43 -0500

    Update header.php

--- src/components/header.php
+++ src/components/header.php
@@ -6,6 +6,8 @@
 
 <a href="/account.php" style="color: white !important;  ">Account</a>
 <a href="/checkout.php" style="color: white !important;  ">Checkout</a>
+  <a href="/cart.php" style="color: white !important;  ">Bag</a>
+  <a href="/login.php" style="color: white !important;  ">Log In</a>
 <a href="/logout.php" style="color: white !important;  ">Log out</a>
 
 </header>
